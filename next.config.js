ab92e74deee7f16eaf9d3fc7ecdea83d50d31d58
Danielle <danielle.gribben@gmail.com>
2026-09-02 13:37:01 +0100

    Initial commit of Next.js portfolio starter kit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// This file runs the the default configuration of Next.js but you can customise it to your needs. 
+// For example, you can add custom webpack configuration, environment variables, or other settings that affect the build process.
+// Next.js documentation: https://nextjs.org/docs/api-reference/next.config.js/introduction
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+}
+
+module.exports = nextConfig
